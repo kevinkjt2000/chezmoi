9a8f8a1718f2ad0c96d5b6d1f3e87784f6d2fd9d
Tom Payne <twpayne@gmail.com>
2020-11-21 04:19:05 +0100

    Allow install script to exec chezmoi and make it more robust

--- assets/scripts/install.sh
+++ assets/scripts/install.sh
@@ -4,33 +4,30 @@ set -e
 usage() {
 	this=$1
 	cat <<EOF
-$this: download go binaries for twpayne/chezmoi
+$this: download chezmoi and optionally run chezmoi
 
-Usage: $this [-b] bindir [-d] [tag]
-  -b sets bindir or installation directory, Defaults to ./bin
+Usage: $this [-b bindir] [-d] [-t tag] [-- chezmoi-args...]
+  -b sets the installation directory, default is ./bin
   -d turns on debug logging
-   [tag] is a tag from
-   https://github.com/twpayne/chezmoi/releases
-   If tag is missing, then the latest will be used.
+  -t sets the tag from https://github.com/twpayne/chezmoi/releases, default is latest
+If chezmoi-args is given, chezmoi is run with chezmoi-args.
 EOF
 	exit 2
 }
 
 parse_args() {
-	#BINDIR is ./bin unless set be ENV
-	# over-ridden by flag below
-
 	BINDIR=${BINDIR:-./bin}
-	while getopts "b:dh?x" arg; do
-		case "$arg" in
-		b) BINDIR="$OPTARG" ;;
+	while getopts "b:dh?t:x" arg; do
+		case "${arg}" in
+		b) BINDIR="${OPTARG}" ;;
 		d) log_set_priority 10 ;;
 		h | \?) usage "$0" ;;
+		t) TAG="${OPTARG}" ;;
 		x) set -x ;;
 		esac
 	done
 	shift $((OPTIND - 1))
-	TAG=$1
+	EXECARGS="$*"
 }
 # this function wraps all the destructive operations
 # if a curl|bash cuts off the end of the script due to
@@ -42,52 +39,40 @@ execute() {
 	http_download "${tmpdir}/${TARBALL}" "${TARBALL_URL}"
 	http_download "${tmpdir}/${CHECKSUM}" "${CHECKSUM_URL}"
 	hash_sha256_verify "${tmpdir}/${TARBALL}" "${tmpdir}/${CHECKSUM}"
-	srcdir="${tmpdir}"
 	(cd "${tmpdir}" && untar "${TARBALL}")
 	test ! -d "${BINDIR}" && install -d "${BINDIR}"
-	for binexe in $BINARIES; do
-		if [ "$OS" = "windows" ]; then
-			binexe="${binexe}.exe"
-		fi
-		install "${srcdir}/${binexe}" "${BINDIR}/"
-		log_info "installed ${BINDIR}/${binexe}"
-	done
+	BINEXE="${BINARY}"
+	if [ "${OS}" = "windows" ]; then
+		BINEXE="${BINEXE}.exe"
+	fi
+	install "${tmpdir}/${BINEXE}" "${BINDIR}/"
+	log_info "installed ${BINDIR}/${BINEXE}"
 	rm -rf "${tmpdir}"
+	# shellcheck disable=SC2086
+	test -n "${EXECARGS}" && exec "${BINDIR}/${BINEXE}" ${EXECARGS}
 }
-get_binaries() {
-	case "$PLATFORM" in
-	darwin/amd64) BINARIES="chezmoi" ;;
-	darwin/arm) BINARIES="chezmoi" ;;
-	darwin/arm64) BINARIES="chezmoi" ;;
-	darwin/ppc64) BINARIES="chezmoi" ;;
-	darwin/ppc64le) BINARIES="chezmoi" ;;
-	freebsd/386) BINARIES="chezmoi" ;;
-	freebsd/amd64) BINARIES="chezmoi" ;;
-	freebsd/arm) BINARIES="chezmoi" ;;
-	freebsd/arm64) BINARIES="chezmoi" ;;
-	freebsd/ppc64) BINARIES="chezmoi" ;;
-	freebsd/ppc64le) BINARIES="chezmoi" ;;
-	linux/386) BINARIES="chezmoi chezmoi" ;;
-	linux/amd64) BINARIES="chezmoi chezmoi" ;;
-	linux/arm) BINARIES="chezmoi" ;;
-	linux/arm64) BINARIES="chezmoi chezmoi" ;;
-	linux/ppc64) BINARIES="chezmoi" ;;
-	linux/ppc64le) BINARIES="chezmoi" ;;
-	openbsd/386) BINARIES="chezmoi" ;;
-	openbsd/amd64) BINARIES="chezmoi" ;;
-	openbsd/arm) BINARIES="chezmoi" ;;
-	openbsd/arm64) BINARIES="chezmoi" ;;
-	openbsd/ppc64) BINARIES="chezmoi" ;;
-	openbsd/ppc64le) BINARIES="chezmoi" ;;
-	windows/386) BINARIES="chezmoi" ;;
-	windows/amd64) BINARIES="chezmoi" ;;
-	windows/arm) BINARIES="chezmoi" ;;
-	windows/arm64) BINARIES="chezmoi" ;;
-	windows/ppc64) BINARIES="chezmoi" ;;
-	windows/ppc64le) BINARIES="chezmoi" ;;
+platform_check() {
+	case "${PLATFORM}" in
+	darwin/amd64) return 0 ;;
+	freebsd/386) return 0 ;;
+	freebsd/amd64) return 0 ;;
+	freebsd/arm) return 0 ;;
+	freebsd/arm64) return 0 ;;
+	linux/386) return 0 ;;
+	linux/amd64) return 0 ;;
+	linux/arm) return 0 ;;
+	linux/arm64) return 0 ;;
+	linux/ppc64) return 0 ;;
+	linux/ppc64le) return 0 ;;
+	openbsd/386) return 0 ;;
+	openbsd/amd64) return 0 ;;
+	openbsd/arm) return 0 ;;
+	openbsd/arm64) return 0 ;;
+	windows/386) return 0 ;;
+	windows/amd64) return 0 ;;
 	*)
-		log_crit "platform $PLATFORM is not supported.  Make sure this script is up-to-date and file request at https://github.com/${PREFIX}/issues/new"
-		exit 1
+		log_crit "platform ${PLATFORM} is not supported.  Make sure this script is up-to-date and file request at https://github.com/${PREFIX}/issues/new"
+		return 1
 		;;
 	esac
 }
@@ -97,32 +82,32 @@ tag_to_version() {
 	else
 		log_info "checking GitHub for tag '${TAG}'"
 	fi
-	REALTAG=$(github_release "$OWNER/$REPO" "${TAG}") && true
-	if test -z "$REALTAG"; then
+	REALTAG=$(github_release "${OWNER}/${REPO}" "${TAG}") && true
+	if test -z "${REALTAG}"; then
 		log_crit "unable to find '${TAG}' - use 'latest' or see https://github.com/${PREFIX}/releases for details"
 		exit 1
 	fi
 	# if version starts with 'v', remove it
-	TAG="$REALTAG"
+	TAG="${REALTAG}"
 	VERSION=${TAG#v}
 }
 adjust_format() {
 	# change format (tar.gz or zip) based on OS
-	case ${OS} in
+	case "${OS}" in
 	windows) FORMAT=zip ;;
 	esac
 	true
 }
 adjust_os() {
 	# adjust archive name based on OS
-	case ${OS} in
+	case "${OS}" in
 	386) OS=i386 ;;
 	esac
 	true
 }
 adjust_arch() {
 	# adjust archive name based on ARCH
-	case ${ARCH} in
+	case "${ARCH}" in
 	386) ARCH=i386 ;;
 	esac
 	true
@@ -151,13 +136,13 @@ log_set_priority() {
 }
 log_priority() {
 	if test -z "$1"; then
-		echo "$_logp"
+		echo "${_logp}"
 		return
 	fi
-	[ "$1" -le "$_logp" ]
+	[ "$1" -le "${_logp}" ]
 }
 log_tag() {
-	case $1 in
+	case "$1" in
 	0) echo "emerg" ;;
 	1) echo "alert" ;;
 	2) echo "crit" ;;
@@ -187,30 +172,30 @@ log_crit() {
 }
 uname_os() {
 	os=$(uname -s | tr '[:upper:]' '[:lower:]')
-	case "$os" in
+	case "${os}" in
 	cygwin_nt*) os="windows" ;;
 	mingw*) os="windows" ;;
 	msys_nt*) os="windows" ;;
 	esac
-	echo "$os"
+	echo "${os}"
 }
 uname_arch() {
 	arch=$(uname -m)
-	case $arch in
+	case "${arch}" in
 	x86_64) arch="amd64" ;;
 	x86) arch="386" ;;
 	i686) arch="386" ;;
 	i386) arch="386" ;;
 	aarch64) arch="arm64" ;;
-	armv5*) arch="armv5" ;;
-	armv6*) arch="armv6" ;;
-	armv7*) arch="armv7" ;;
+	armv5*) arch="arm" ;;
+	armv6*) arch="arm" ;;
+	armv7*) arch="arm" ;;
 	esac
-	echo ${arch}
+	echo "${arch}"
 }
 uname_os_check() {
 	os=$(uname_os)
-	case "$os" in
+	case "${os}" in
 	darwin) return 0 ;;
 	dragonfly) return 0 ;;
 	freebsd) return 0 ;;
@@ -223,18 +208,16 @@ uname_os_check() {
 	solaris) return 0 ;;
 	windows) return 0 ;;
 	esac
-	log_crit "uname_os_check '$(uname -s)' got converted to '$os' which is not a GOOS value. Please file bug at https://github.com/client9/shlib"
+	log_crit "uname_os_check '$(uname -s)' got converted to '${os}' which is not a GOOS value. Please file bug at https://github.com/client9/shlib"
 	return 1
 }
 uname_arch_check() {
 	arch=$(uname_arch)
-	case "$arch" in
+	case "${arch}" in
 	386) return 0 ;;
 	amd64) return 0 ;;
 	arm64) return 0 ;;
-	armv5) return 0 ;;
-	armv6) return 0 ;;
-	armv7) return 0 ;;
+	arm) return 0 ;;
 	ppc64) return 0 ;;
 	ppc64le) return 0 ;;
 	mips) return 0 ;;
@@ -244,14 +227,14 @@ uname_arch_check() {
 	s390x) return 0 ;;
 	amd64p32) return 0 ;;
 	esac
-	log_crit "uname_arch_check '$(uname -m)' got converted to '$arch' which is not a GOARCH value.  Please file bug report at https://github.com/client9/shlib"
+	log_crit "uname_arch_check '$(uname -m)' got converted to '${arch}' which is not a GOARCH value.  Please file bug report at https://github.com/client9/shlib"
 	return 1
 }
 untar() {
 	tarball=$1
 	case "${tarball}" in
-	*.tar.gz | *.tgz) tar --no-same-owner -xzf "${tarball}" ;;
-	*.tar) tar --no-same-owner -xf "${tarball}" ;;
+	*.tar.gz | *.tgz) tar -xzf "${tarball}" ;;
+	*.tar) tar -xf "${tarball}" ;;
 	*.zip) unzip "${tarball}" ;;
 	*)
 		log_err "untar unknown archive format for ${tarball}"
@@ -263,13 +246,13 @@ http_download_curl() {
 	local_file=$1
 	source_url=$2
 	header=$3
-	if [ -z "$header" ]; then
-		code=$(curl -w '%{http_code}' -sL -o "$local_file" "$source_url")
+	if [ -z "${header}" ]; then
+		code=$(curl -w '%{http_code}' -sL -o "${local_file}" "${source_url}")
 	else
-		code=$(curl -w '%{http_code}' -sL -H "$header" -o "$local_file" "$source_url")
+		code=$(curl -w '%{http_code}' -sL -H "${header}" -o "${local_file}" "${source_url}")
 	fi
-	if [ "$code" != "200" ]; then
-		log_debug "http_download_curl received HTTP status $code"
+	if [ "${code}" != "200" ]; then
+		log_debug "http_download_curl received HTTP status ${code}"
 		return 1
 	fi
 	return 0
@@ -278,10 +261,10 @@ http_download_wget() {
 	local_file=$1
 	source_url=$2
 	header=$3
-	if [ -z "$header" ]; then
-		wget -q -O "$local_file" "$source_url"
+	if [ -z "${header}" ]; then
+		wget -q -O "${local_file}" "${source_url}"
 	else
-		wget -q --header "$header" -O "$local_file" "$source_url"
+		wget -q --header "${header}" -O "${local_file}" "${source_url}"
 	fi
 }
 http_download() {
@@ -299,56 +282,59 @@ http_download() {
 http_copy() {
 	tmp=$(mktemp)
 	http_download "${tmp}" "$1" "$2" || return 1
-	body=$(cat "$tmp")
+	body=$(cat "${tmp}")
 	rm -f "${tmp}"
-	echo "$body"
+	echo "${body}"
 }
 github_release() {
 	owner_repo=$1
 	version=$2
-	test -z "$version" && version="latest"
+	test -z "${version}" && version="latest"
 	giturl="https://github.com/${owner_repo}/releases/${version}"
-	json=$(http_copy "$giturl" "Accept:application/json")
-	test -z "$json" && return 1
-	version=$(echo "$json" | tr -s '\n' ' ' | sed 's/.*"tag_name":"//' | sed 's/".*//')
-	test -z "$version" && return 1
-	echo "$version"
+	json=$(http_copy "${giturl}" "Accept:application/json")
+	test -z "${json}" && return 1
+	version=$(echo "${json}" | tr -s '\n' ' ' | sed 's/.*"tag_name":"//' | sed 's/".*//')
+	test -z "${version}" && return 1
+	echo "${version}"
 }
 hash_sha256() {
-	TARGET=${1:-/dev/stdin}
+	target=${1:-/dev/stdin}
 	if is_command gsha256sum; then
-		hash=$(gsha256sum "$TARGET") || return 1
-		echo "$hash" | cut -d ' ' -f 1
+		hash=$(gsha256sum "${target}") || return 1
+		echo "${hash}" | cut -d ' ' -f 1
 	elif is_command sha256sum; then
-		hash=$(sha256sum "$TARGET") || return 1
-		echo "$hash" | cut -d ' ' -f 1
+		hash=$(sha256sum "${target}") || return 1
+		echo "${hash}" | cut -d ' ' -f 1
 	elif is_command shasum; then
-		hash=$(shasum -a 256 "$TARGET" 2>/dev/null) || return 1
-		echo "$hash" | cut -d ' ' -f 1
+		hash=$(shasum -a 256 "${target}" 2>/dev/null) || return 1
+		echo "${hash}" | cut -d ' ' -f 1
+	elif is_command sha256; then
+		hash=$(sha256 -q "${target}" 2>/dev/null) || return 1
+		echo "${hash}" | cut -d ' ' -f 1
 	elif is_command openssl; then
-		hash=$(openssl -dst openssl dgst -sha256 "$TARGET") || return 1
-		echo "$hash" | cut -d ' ' -f a
+		hash=$(openssl -dst openssl dgst -sha256 "${target}") || return 1
+		echo "${hash}" | cut -d ' ' -f a
 	else
 		log_crit "hash_sha256 unable to find command to compute sha-256 hash"
 		return 1
 	fi
 }
 hash_sha256_verify() {
-	TARGET=$1
+	target=$1
 	checksums=$2
-	if [ -z "$checksums" ]; then
+	if [ -z "${checksums}" ]; then
 		log_err "hash_sha256_verify checksum file not specified in arg2"
 		return 1
 	fi
-	BASENAME=${TARGET##*/}
-	want=$(grep "${BASENAME}" "${checksums}" 2>/dev/null | tr '\t' ' ' | cut -d ' ' -f 1)
-	if [ -z "$want" ]; then
-		log_err "hash_sha256_verify unable to find checksum for '${TARGET}' in '${checksums}'"
+	basename=${target##*/}
+	want=$(grep "${basename}" "${checksums}" 2>/dev/null | tr '\t' ' ' | cut -d ' ' -f 1)
+	if [ -z "${want}" ]; then
+		log_err "hash_sha256_verify unable to find checksum for '${target}' in '${checksums}'"
 		return 1
 	fi
-	got=$(hash_sha256 "$TARGET")
-	if [ "$want" != "$got" ]; then
-		log_err "hash_sha256_verify checksum for '$TARGET' did not verify ${want} vs $got"
+	got=$(hash_sha256 "${target}")
+	if [ "${want}" != "${got}" ]; then
+		log_err "hash_sha256_verify checksum for '$target' did not verify ${want} vs $got"
 		return 1
 	fi
 }
@@ -358,28 +344,28 @@ End of functions from https://github.com/client9/shlib
 ------------------------------------------------------------------------
 EOF
 
-PROJECT_NAME="chezmoi"
+PROJECT_NAME=chezmoi
 OWNER=twpayne
-REPO="chezmoi"
+REPO=chezmoi
 BINARY=chezmoi
 FORMAT=tar.gz
 OS=$(uname_os)
 ARCH=$(uname_arch)
-PREFIX="$OWNER/$REPO"
+PREFIX="${OWNER}/${REPO}"
 
 # use in logging routines
 log_prefix() {
-	echo "$PREFIX"
+	echo "${PREFIX}"
 }
 PLATFORM="${OS}/${ARCH}"
 GITHUB_DOWNLOAD=https://github.com/${OWNER}/${REPO}/releases/download
 
-uname_os_check "$OS"
-uname_arch_check "$ARCH"
+uname_os_check "${OS}"
+uname_arch_check "${ARCH}"
 
 parse_args "$@"
 
-get_binaries
+platform_check
 
 tag_to_version
 
